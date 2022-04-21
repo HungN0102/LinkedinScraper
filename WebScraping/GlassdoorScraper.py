c8f1f14cb590e686f5d468557aa74e30e3a8a2cf
HungN0102 <66207167+HungN0102@users.noreply.github.com>
2022-04-21 20:42:46 +0100

    Fixed: Fixed duplicate

--- WebScraping/GlassdoorScraper.py
+++ WebScraping/GlassdoorScraper.py
@@ -1,23 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
-import numpy as np
-import re
+from selenium import webdriver
 import pandas as pd
+import time
 
 class GlassdoorScraper:
-    def __init__(self,url,pages):
-        self.url = url[:-4]
-        self.pages = pages
+    def __init__(self,url):
+        self.url = url
         self.URL = None
         self.rows = []
         self.warnings = []
         self.full_link = None
+        self.driver = None
 
-    def get_soup(self,url):
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
-        source = requests.get(url, headers=headers)
-        soup = BeautifulSoup(source.content, 'html.parser')
-        return soup
+    def open_website(self):
+        PATH = "C:\Program Files (x86)\chromedriver.exe"
+        self.driver = webdriver.Chrome(PATH)
+        self.driver.get(self.url)
 
     def get_link(self,job_tag):
         link_origin = "https://www.glassdoor.co.uk/partner/jobListing.htm?"
@@ -26,54 +25,85 @@ class GlassdoorScraper:
         full_link = link_origin + link_destination
         return full_link
 
+    def get_soup(self):
+        # Get bs4 soup
+        src = self.driver.page_source
+        soup = BeautifulSoup(src, 'html.parser')
+        return soup
+
+    def get_soup4(self,url):
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
+        source = requests.get(url, headers=headers)
+        soup = BeautifulSoup(source.content, 'html.parser')
+        return soup
+
+    def next_page(self):
+        element = self.driver.find_element_by_xpath("//ul/li[29]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        self.driver.find_element_by_class_name("nextButton").click()
+        time.sleep(4)
+
+        try:
+            self.driver.find_element_by_xpath("//span[@class='SVGInline modal_closeIcon']").click()
+        except:
+            pass
+
     def get_data(self, li_class = 'react-job-listing',
                  company_class="d-flex justify-content-between align-items-start",
-                 job_class="jobLink job-search-key-1rd3saf eigr9kq1",
-                 location_class="css-1buaf54 pr-xxsm job-search-key-iii9i8 e1rrn5ka4",
-                 old_class = "d-flex align-items-end pl-std css-17n8uzw",
+                 job_class="job-link",
+                 location_class="css-1buaf54",
+                 old_class = "job-age",
                  salary_class = "detailSalary",
                  detail_class = "css-1yuy9gt ecgq1xb4"):
 
-        for i in range(2, self.pages + 1):
-            if i == 1:
-                self.URL = self.url + '.htm'
-            else:
-                self.URL = self.url + '_IP' + str(i) + '.htm'
+        soup = self.get_soup()
+        matches = soup.find_all('li', class_=li_class)
+        for match in matches:
+            company = match.find("div", class_=company_class)
+            company = company.get_text(separator=" ").strip()
 
-            soup = self.get_soup(self.URL)
-            matches = soup.find_all('li', class_=li_class)
-            for match in matches:
-                company = match.find("div", class_=company_class)
-                company = company.get_text(separator=" ").strip()
+            job_title = match.find("a", {"data-test":job_class})
+            job_title = job_title.get_text(separator=" ").strip()
 
-                job_title = match.find("a", class_=job_class)
-                job_title = job_title.get_text(separator=" ").strip()
+            location = match.find("span", class_=location_class)
+            location = location.get_text(separator=" ").strip()
 
-                location = match.find("span", class_=location_class)
-                location = location.get_text(separator=" ").strip()
+            old = match.find("div", {"data-test":old_class})
+            old = old.get_text(separator=" ").strip()
 
-                old = match.find("div", class_=old_class)
-                old = old.get_text(separator=" ").strip()
+            try:
+                salary = match.find("span", {"data-test": salary_class})
+                salary = salary.get_text(separator=" ").strip()
+            except Exception as e:
+                self.warnings.append(e)
 
-                try:
-                    salary = match.find("span", {"data-test": salary_class})
-                    salary = salary.get_text(separator=" ").strip()
-                except Exception as e:
-                    self.warnings.append(e)
-
-                self.full_link = self.get_link(match.find("a", class_=job_class))
-                soup2 = self.get_soup(self.full_link)
-                job_details = soup2.find("div", class_=detail_class).get_text(separator=" ").strip()
-                self.rows.append([self.full_link, job_title, company, location, old, salary, job_details])
+            self.full_link = self.get_link(match.find("a",{"data-test":job_class}))
+            soup2 = self.get_soup4(self.full_link)
+            job_details = soup2.find("div", class_=detail_class).get_text(separator=" ").strip()
+            self.rows.append([self.full_link, job_title, company, location, old, salary, job_details])
 
             print(len(self.rows))
-if __name__ == "__main__":
-    url = "https://www.glassdoor.co.uk/Job/london-marketing-jobs-SRCH_IL.0,6_IC2671300_KO7,16.htm"
-    pages = 25
 
-    bot = GlassdoorScraper(url,pages)
-    bot.get_data(detail_class="css-1yuy9gt ecgq1xb4")
+if __name__ == "__main__":
+    url = "https://www.glassdoor.co.uk/Job/london-data-engineer-jobs-SRCH_IL.0,6_IC2671300_KO7,20.htm"
+    pages = 25
+    print("Starting")
+    bot = GlassdoorScraper(url)
+    bot.open_website()
+
+    for i in range(pages):
+        bot.get_data(detail_class="css-1yuy9gt ecgq1xb4")
+        bot.next_page()
 
     df = pd.DataFrame(bot.rows, columns = ["url", "job_title", "company_name", "location", "old", "salary", "job_details"])
+    print("Finished")
 
-# df.to_csv("glassdoor_swe.csv", encoding='utf-8-sig')
+# df.to_csv("glassdoor_accountant.csv", encoding='utf-8-sig')
+
+# soup = bot.get_soup()
+# matches = soup.find_all('li', class_='react-job-listing')
+# match = matches[0]
+#
+# match.find("span", class_="css-1buaf54").text
+
+df
